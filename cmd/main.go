ea5089a969fd74b27b7497d973d377dd66606038
genearl <generals.space@gmail.com>
2020-03-23 11:15:35 +0800

    更新: 信号处理机制与处理函数分离

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,19 @@ import (
 
 	"github.com/generals-space/crd-ipkeeper/pkg/server"
 	"github.com/generals-space/crd-ipkeeper/pkg/signals"
+	"github.com/generals-space/crd-ipkeeper/pkg/util"
 )
+
+// stopHandler 收到退出信号清理 cniserver.sock 文件.
+func stopHandler(sockPath string) {
+	if util.Exists(sockPath) {
+		err := os.Remove(sockPath)
+		if err != nil {
+			klog.Errorf("failed to remove cniserver sock file: %s", err)
+		}
+	}
+	return
+}
 
 func main() {
 	klog.SetOutput(os.Stdout)
@@ -19,7 +31,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	signals.SetupSignalHandler()
+	signals.SetupSignalHandler(stopHandler, config.BindSocket)
 	server.NewController(config)
 	server.RunServer(config)
 }
--- pkg/signals/signal.go
+++ pkg/signals/signal.go
@@ -4,35 +4,22 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-
-	"k8s.io/klog"
-
-	"github.com/generals-space/crd-ipkeeper/pkg/util"
 )
 
 var shutdownSignals = []os.Signal{syscall.SIGINT, syscall.SIGTERM}
 
 // SetupSignalHandler 设置信号处理机制, 但不涉及具体的处理操作.
 // 具体的处理操作需要通过 fn 参数传入.
-func SetupSignalHandler() {
+func SetupSignalHandler(fn func(string), arg string) {
 	// sigCh接收信号, 注意之后的清理操作有可能失败, 失败后不能直接退出.
 	sigCh := make(chan os.Signal, 1)
 	// 一般delete pod时, 收到的是SIGTERM信号.
 	signal.Notify(sigCh, shutdownSignals...)
-	var sockPath = "/var/run/cniserver.sock"
 
 	go func() {
-		var err error
-
 		<-sigCh
-
-		if util.Exists(sockPath) {
-			err = os.Remove(sockPath)
-			if err != nil {
-				klog.Errorf("failed to remove cniserver sock file: %s", err)
-			}
-		}
-
+		// 调用fn(), 执行真正的清理工作.
+		fn(arg)
 		os.Exit(1)
 	}()
 }
